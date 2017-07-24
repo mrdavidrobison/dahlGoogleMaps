62c459537c563d94af5f02654559a0c672ed741c
david robison <mrdavidrobison@gmail.com>
2017-07-24 16:14:46 -0600

    create address after clicking markers

--- scripts/index.js
+++ scripts/index.js
@@ -6,30 +6,60 @@ function initMap() {
     zoom: 13
   });
 
+  // create event listener and message after click markers
+  function attachSecretMessage(marker, secretMessage) {
+    var infowindow = new google.maps.InfoWindow({
+      content: secretMessage
+    });
+
+    marker.addListener('click', function () {
+      infowindow.open(marker.get('map'), marker);
+    });
+  }
+
+  // create array of addresses for each marker
+  var secretMessages = ['This', 'is', 'the', 'secret', 'message'];
+
+  // create icon and properties
+  var icon = {
+    url: "google map marker.png",
+    scaledSize: new google.maps.Size(50, 50),
+  };
+
   // create markers for each property listing
   var estrella = new google.maps.Marker({
     position: new google.maps.LatLng(27.928814, -82.489407),
     map: map,
-    icon: "google map marker.png"
+    icon: icon
   });
+  attachSecretMessage(estrella, secretMessages[i]);
+
   var juanita = new google.maps.Marker({
     position: new google.maps.LatLng(27.868068, -82.529975),
     map: map,
-    icon: "google map marker.png"
+    icon: icon
   });
+  attachSecretMessage(juanita, secretMessages[i]);
+
   var luna = new google.maps.Marker({
     position: new google.maps.LatLng(27.928471, -82.491106),
     map: map,
-    icon: "google map marker.png"
+    icon: icon
   });
+  attachSecretMessage(luna, secretMessages[i]);
+
   var feliz = new google.maps.Marker({
     position: new google.maps.LatLng(27.928510, -82.492345),
     map: map,
-    icon: "google map marker.png"
+    icon: icon
   });
+  attachSecretMessage(feliz, secretMessages[i]);
+  
   var bella = new google.maps.Marker({
     position: new google.maps.LatLng(27.866954, -82.524223),
     map: map,
-    icon: "google map marker.png"
+    icon: icon
   });
+  attachSecretMessage(bella, secretMessages[i]);
+
 }

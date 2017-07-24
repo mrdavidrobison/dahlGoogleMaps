42c011672cb1ffd9c29aeaa266a6f8584e757f73
david robison <mrdavidrobison@gmail.com>
2017-07-24 15:22:12 -0600

    include long and lat for five rental properties

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 27.899, lng: -82.515},
+  var bella = {lat: 27.866954, lng: -82.524223};
+  var estrella = {lat: 27.928814, lng: -82.489407};
+  var juanita = {lat: 27.868068, lng: -82.529975};
+  var luna = {lat: 27.928471, lng: -82.491106};
+  var bella = {lat: 27.866954, lng: -82.524223};
+  var bella = {lat: 27.866954, lng: -82.524223};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 27.899, lng: -82.515 },
     zoom: 13
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
   });
 }
